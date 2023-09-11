70f9ef05665f869e55dd15467a6812692b51b0a8
David Rodriguez-Jenkins <53097291+davidrodjen@users.noreply.github.com>
2023-09-11 03:00:05 +0000

    Submission HOS10 DavidRodJen

---
+++ HOS10/frontend/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="yourBackendURL/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
